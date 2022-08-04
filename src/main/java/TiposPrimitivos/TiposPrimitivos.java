c3d7be33ecc6190b9b10afaa18de7becb45c5ee5
cutioluis <cutioluis@gmail.com>
2022-08-04 17:02:31 -0500

    tipos de datos: short, int, long

--- src/main/java/TiposPrimitivos/TiposPrimitivos.java
+++ src/main/java/TiposPrimitivos/TiposPrimitivos.java
@@ -10,6 +10,20 @@ public class TiposPrimitivos {
       System.out.println("alcance maximo" + Byte.MAX_VALUE);
       System.out.println("alcance minimo" + Byte.MIN_VALUE);
 
+      short numeroShort = 13;
+      System.out.println(numeroShort);
+      System.out.println("alcance maximo" + Short.MAX_VALUE);
+      System.out.println("alcance minimo" + Short.MIN_VALUE);
 
+      int numeroInt = 4214;
+      System.out.println(numeroInt);
+      System.out.println("alcance maximo" + Integer.MAX_VALUE);
+      System.out.println("alcance maximo" + Integer.MIN_VALUE);
+
+      // Podemos convertir el tipo de dato asi
+      long numeroLong = (long)4213213;
+      System.out.println(numeroLong);
+      System.out.println("alcance maximo" + Long.MAX_VALUE);
+      System.out.println("alcance maximo" + Long.MIN_VALUE);
    }
 }
