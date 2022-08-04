6b1ca37d7bbc62e3b0fb803d76c0301c9816c6aa
cutioluis <cutioluis@gmail.com>
2022-08-04 16:50:30 -0500

    tipos datos: byte

---
+++ src/main/java/TiposPrimitivos/TiposPrimitivos.java
@@ -0,0 +1,15 @@
+package TiposPrimitivos;
+
+public class TiposPrimitivos {
+   public static void main(String[] args) {
+      // Parar entero: byte = 8bits, short = 16bits, int = 32bits, long = 64bits
+      byte numeroByte = 54;
+      System.out.println(numeroByte);
+
+      // Podemos ver el tupo el alcanze maximo que cada tipo de dato
+      System.out.println("alcance maximo" + Byte.MAX_VALUE);
+      System.out.println("alcance minimo" + Byte.MIN_VALUE);
+
+
+   }
+}
