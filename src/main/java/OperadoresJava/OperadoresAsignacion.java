ac9cabba3d484ceaed193c9e9faa91db17f37ac6
cutioluis <cutioluis@gmail.com>
2022-08-08 18:19:23 -0500

    operadores: asignacion

---
+++ src/main/java/OperadoresJava/OperadoresAsignacion.java
@@ -0,0 +1,25 @@
+package OperadoresJava;
+
+public class OperadoresAsignacion {
+   public static void main(String[] args) {
+      int a = 3;
+      int b = 3;
+      System.out.println(a == b);
+
+      a += 3; // a = a + 3
+      System.out.println(a);
+
+      a -= 3;// a = a - 3
+      System.out.println(a);
+
+      a *= 3; // a = a * 3
+      System.out.println(a);
+
+      a /= 3; // a = a / 3
+      System.out.println(a);
+
+      a &= 3; // a = a & 3
+      System.out.println(a);
+   }
+
+}
