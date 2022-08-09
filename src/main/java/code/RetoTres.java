2ffe486ee5e97108fc22126287f86064dc439cd4
cutioluis <cutioluis@gmail.com>
2022-08-09 12:11:10 -0500

    retoTres: mayor o menor

---
+++ src/main/java/code/RetoTres.java
@@ -0,0 +1,22 @@
+package code;
+
+import java.util.Scanner;
+
+public class RetoTres {
+   public static void main(String[] args) {
+      Scanner i = new Scanner(System.in);
+
+      System.out.println("Dame un numero");
+      int a = i.nextInt();
+
+      System.out.println("Dame otro numero");
+      int b = i.nextInt();
+
+      if (a > b) {
+         System.out.println("Es mayor");
+      } else {
+         System.out.println("Es menor");
+      }
+
+   }
+}
