d48cf8ca2b710c383c6b9880ccb966d0bce0544d
cutioluis <cutioluis@gmail.com>
2022-08-09 15:18:37 -0500

    estructuras control: if, switch

---
+++ src/main/java/SentenciasControl/SentenciaIfElse.java
@@ -0,0 +1,18 @@
+package SentenciasControl;
+
+public class SentenciaIfElse {
+   public static void main(String[] args) {
+      // Sentencia if
+      boolean condicion = false;
+
+      // Recive la condicion si es true la ejecuta el codigo 
+      // Si no es true no ejecuta el otro ocdigo
+
+      if (condicion) {
+         System.out.println("Si la condicion es true imprime este mensaje");
+      } else {
+         System.out.println("Si no es true no immrime esto");
+      }
+   }
+   
+}
---
+++ src/main/java/SentenciasControl/SentenciaSwitch.java
@@ -0,0 +1,48 @@
+package SentenciasControl;
+
+public class SentenciaSwitch {
+   public static void main(String[] args) {
+
+      int n = 2;
+
+      switch (n) // expresion
+      {
+         case 1:
+            System.out.println("Numero 1");
+            break;
+
+         case 2:
+            System.out.println("Numero 2");
+            break;
+
+         case 3:
+            System.out.println("Numero 3");
+            break;
+      }
+
+      int day = 5;
+      String dayString;
+
+      switch (day) {
+         case 1:
+            dayString = "Monday";
+            break;
+
+         case 2:
+            dayString = "Tusday";
+            break;
+
+         case 3:
+            dayString = "Wednesday";
+            break;
+         case 4:
+            dayString = "Thursday";
+            break;
+         case 5:
+            dayString = "Friday";
+            break;
+      }
+
+   }
+
+}
