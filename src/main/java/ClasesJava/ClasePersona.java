10f47e6d408dcf3d1ba3d2ceb86be8676ca438ef
cutioluis <cutioluis@gmail.com>
2022-08-10 16:37:02 -0500

    clases: fundamentos basicos

---
+++ src/main/java/ClasesJava/ClasePersona.java
@@ -0,0 +1,27 @@
+package ClasesJava;
+
+public class ClasePersona {
+
+   // Diagrama: resources/clases-objetos.png
+   /*
+      * Clase:
+      * - nombre de la clase : Persona
+      * - atributos: nombre, genero, ocupacion
+      * - metodos:
+         * - obtenerName()
+         * - obtenerGenero()
+         * - obtenerOcupacion()
+      * 
+   */
+
+   String nombre;
+   String  apellido;
+
+   public void deployInformation() {
+      System.out.println("Nombre: " + nombre);
+      System.out.println("Apellido: " + apellido);
+   }
+   
+   
+
+}
---
+++ src/main/java/ClasesJava/PruebaPersona.java
@@ -0,0 +1,28 @@
+package ClasesJava;
+
+public class PruebaPersona {
+   public static void main(String[] args) {
+      // Crearemos objetos a partir de la clase Persona
+
+
+      // Crearemos nuestro ojeto, y tiene una referencia en memoria
+      ClasePersona persona1 = new ClasePersona();
+
+      // Atributos
+      persona1.nombre = "Luis";
+      persona1.apellido = "Cutio";
+
+      // metodos
+      persona1.deployInformation();
+
+      ClasePersona persona2 = new ClasePersona();
+      persona2.nombre = "Alex";
+      persona2.apellido = "Cutio";
+      persona2.deployInformation();
+   
+      // Referencia de memoria
+      System.out.println(persona1);      
+      System.out.println(persona2);
+
+   }
+}
