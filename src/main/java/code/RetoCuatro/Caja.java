cdae788344de8bfdb8d7eee1405c3f0da8fcb35f
cutioluis <cutioluis@gmail.com>
2022-08-11 15:23:17 -0500

    clases y objetos taller01

---
+++ src/main/java/code/RetoCuatro/Caja.java
@@ -0,0 +1,26 @@
+package code.RetoCuatro;
+
+public class Caja {
+   // Atributos
+   int alto;
+   int ancho;
+   int profundidad;
+
+   // Constructores
+   public Caja() {
+      System.out.println("Constructor iniciado");
+   }
+
+   public int getVolumenCaja() {
+      return ancho * alto * profundidad;
+   }
+
+   public Caja(int alto, int ancho, int profundidad) {
+      this.alto = alto;
+      this.ancho = ancho;
+      this.profundidad = profundidad;
+
+      System.out.println("Ejecuta argumentos");
+   }
+
+}
---
+++ src/main/java/code/RetoCuatro/PruebaCaja.java
@@ -0,0 +1,8 @@
+package code.RetoCuatro;
+
+public class PruebaCaja {
+   public static void main(String[] args) {
+      Caja caja = new Caja(2, 2, 2);
+      System.out.println("Volumen de la caja: " + caja.getVolumenCaja());
+   }
+}
