6af9a2e59b5d2fb22c171406eae8659d2a10e542
cutioluis <cutioluis@gmail.com>
2022-08-11 09:35:29 -0500

    constructores

--- src/main/java/Metodos/Aritmetica.java
+++ src/main/java/Metodos/Aritmetica.java
@@ -5,6 +5,13 @@ public class Aritmetica {
    int a;
    int b;
 
+   public Aritmetica() {
+      //Este es nuestro constructor
+      // Sabemos que se un constructor por que se llama igual que la CLASE
+      // Java lo inicia por default asi que no es necesario que nosotros lo creemos
+      System.out.println("Aqui se inicia el constructuor");
+   }
+ 
    // Metodos
    public void sumar() {
       System.out.println("Resultado Metodo: " + (a + b));
@@ -15,9 +22,9 @@ public class Aritmetica {
    }
 
    public int sumarWithParams(int rg1, int rg2) {
-      a  = rg1;
+      a = rg1;
       a = rg2;
-      return  rg1  + rg2;
+      return rg1 + rg2;
    }
 
    // This
