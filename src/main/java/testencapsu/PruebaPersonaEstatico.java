222a72a53fa96ac1ebc9860e6029d76e22486816
cutioluis <cutioluis@gmail.com>
2022-08-12 15:43:08 -0500

    contexto estatico

--- src/main/java/testencapsu/PruebaPersonaEstatico.java
+++ src/main/java/testencapsu/PruebaPersonaEstatico.java
@@ -6,14 +6,24 @@ public class PruebaPersonaEstatico {
    
    // Es static por que apunta a la carga de clases y no aa la carga de objetos
    public static void main(String[] args) {
-      Persona perosna = new Persona("Nombre");
+      Persona perosna0 = new Persona("Nombre");
       Persona perosna1 = new Persona("Nombre");
 
       
       // Como modificamos el toString() ya no muestra el referencia de memoria
       // Ahora muestra los atributos que definimos en toString()
-      System.out.println("Persona: " + perosna );
+      System.out.println("Persona: " + perosna0 );
       System.out.println("Persona: " + perosna1 );
 
+      System.out.println("-------------------------------------");
+      Imprimir(perosna0);
+      Imprimir(perosna1);
+
    }
+
+   // Tiene que ser de tipo static para que pueda funcionar nuestro metodo
+   public static void Imprimir(Persona persona) {
+      System.out.println("Persona: " + persona);
+   }
+
 }
