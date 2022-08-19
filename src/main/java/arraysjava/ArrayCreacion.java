e8093832124bc7ff73fd57f06f93fed61c5b70f7
cutioluis <cutioluis@gmail.com>
2022-08-19 09:23:53 -0500

    arrays: modificar un array

--- src/main/java/arraysjava/ArrayCreacion.java
+++ src/main/java/arraysjava/ArrayCreacion.java
@@ -3,18 +3,27 @@ package arraysjava;
 public class ArrayCreacion {
    public static void main(String[] args) {
 
+      // * Creacion de Arrays
       // Para crear un array
       // 1.tipo de dato
       // 2.nombre de la variaablecon[]
-      // 3. Creamos new int[Aqui definimos el numero de nuestro array] 
+      // 3. Creamos new int[Aqui definimos el numero de nuestro array]
       // Array hereda desde object la calse padre
-   
+
       String myArray2[] = new String[3];
       System.out.println(myArray2);
-      
+
       int myArray[] = new int[3];
       // Se imprime la direccion de memoria
       System.out.println(myArray);
+
+      // * Modificacion de Arrays
+      // Aceddemos a la posicion del Array en este caos la posicion 0 y le asignamos
+      // un valor
+      myArray[0] = 18;
+      // Imprimimos el numero que modificamos accediendo al valor como el
+      // anteriormente
+      System.out.println("Numero 0: " + myArray[0]);
 
    }
 }
