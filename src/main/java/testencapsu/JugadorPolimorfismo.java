3c62992d5804b76934dc9ef5c307f1b83583dbcc
cutioluis <cutioluis@gmail.com>
2022-08-25 12:50:21 -0500

    Polimorfismo final

--- src/main/java/testencapsu/JugadorPolimorfismo.java
+++ src/main/java/testencapsu/JugadorPolimorfismo.java
@@ -5,19 +5,25 @@ import polimorfismo.Persona;
 
 public class JugadorPolimorfismo {
 
+   // ! Cuando usaoms polimorfismo entra los conceptos
+   // ! herencia > sobrescritura > polimorfismo
    public static void main(String[] args) {
       Persona persona = new Persona("Luis", 122);
-      // System.out.println(persona.obtenerDetalles());
       print(persona);
 
-      Jugador jugador = new Jugador("Luis", 200, 172431321);
-      // System.out.println(jugador.obtenerDetalles());
-      print(jugador);
+      // como podemos ver esta variable tabien puede apuntar a la referencia hijo de
+      // la clase padre
+      persona = new Jugador("Luis", 200, 172431321);
+
+      print(persona);
    }
 
+   // * Basicamente con un mismo tipo podemos ejecutar diferentes metodos de la
    // Esto es polimorfismo donde hacemos un metodo
-   // Mandamos un prametro de Persona 
-   // Llamamos a nuestro metodod obtenerDetalles que se ejecuta de acuerdo a la referencia de memoria que seta apuntando
+   // clase padre o hijos
+   // Mandamos un prametro de Persona
+   // Llamamos a nuestro metodod obtenerDetalles que se ejecuta de acuerdo a la
+   // referencia de memoria que seta apuntando
    public static void print(Persona persona) {
       System.out.println("Perosna de " + persona.obtenerDetalles());
    }
