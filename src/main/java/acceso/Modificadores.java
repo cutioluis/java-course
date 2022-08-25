0a9e23a2e447553f3962c1dbf7efc4f50446d9b7
cutioluis <cutioluis@gmail.com>
2022-08-25 10:11:55 -0500

    modificadores de acceso

---
+++ src/main/java/acceso/Modificadores.java
@@ -0,0 +1,13 @@
+package acceso;
+
+public class Modificadores {
+   /*
+    * Public - clases, variables, metodos, constructor
+    * Default - clases, variables, metodos, constructor
+    * protected - variables, metodos, constructor
+    * private - variables, metodos, constructor
+    */
+   public static void main(String[] args) {
+      System.out.println("Hello World!");
+   }
+}
