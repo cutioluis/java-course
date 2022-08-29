24e472d896fbe2420e3b3db2ac3b65f2afcce9c5
cutioluis <cutioluis@gmail.com>
2022-08-29 17:16:21 -0500

    exceptions: rutimeException

--- src/main/java/error/OperacionExcepcion.java
+++ src/main/java/error/OperacionExcepcion.java
@@ -2,8 +2,14 @@ package error;
 
 // La clase padre es Exception es la clase padre pero la su clase padre es THRAWABlE pero la mayoria herenda de EXCEPTION
 // Cuando heredamos de Exception es CheclException
-public class OperacionExcepcion extends Exception {
-   public OperacionExcepcion(String mensaje ) {
+
+//* Usando RuntimeException no se necesita reportar haciendo el try, catch */
+/* Los mas recomendable es usar el RuntimeException POR QUE DEJA CODIGO MAS LIMPIO */
+/*
+ ? Pero si necesitas hacer controlar la exception mas a profundidad usa Exception
+ */
+public class OperacionExcepcion extends RuntimeException {
+   public OperacionExcepcion(String mensaje) {
       super(mensaje);
    }
 }
